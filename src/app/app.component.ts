101452cf0e417e4844d2f585fb363ca5a54356c9
Gustavo de Paula <gdepaula@gmail.com>
2024-06-18 10:53:31 -0300

    Hands on 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AdminRoutingModule } from './components/admin/admin-routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AdminRoutingModule],
   template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent {}
---
+++ src/app/components/admin/admin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, AdminRoutingModule, HomeComponent],
+})
+export class AdminModule {}
