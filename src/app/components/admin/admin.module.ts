053b7a17ac880222238b0012f27964d45238abe1
Gustavo de Paula <gdepaula@gmail.com>
2024-06-26 10:01:51 -0300

    Hands on 11

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AdminRoutingModule, HomeComponent],
+  imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
--- src/app/components/admin/pages/home/home.component.ts
+++ src/app/components/admin/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  constructor(private authService: AuthService) {}
 
+  public logout() {
+    this.authService.logout();
+  }
 }
