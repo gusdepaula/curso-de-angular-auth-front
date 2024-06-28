779a73033c8e6803d282c68302a268dd9b4c06e4
Gustavo de Paula <gdepaula@gmail.com>
2024-06-28 10:06:26 -0300

    IMPORTANTE SOBRE ANGULAR 16

---
+++ src/app/core/guard/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuardGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  if (!authService.isAuthenticated()) {
+    router.navigate(['']);
+    return false;
+  }
+
+  return true;
+};
